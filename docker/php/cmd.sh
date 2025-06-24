b01c7ccbbd8ac3f0634a54a5ad653fe7d476c356
Kishor Rajbanshi <kishorrajbanshi00@gmail.com>
2025-06-24 21:17:09 +0545

    WIP

--- docker/php/cmd.sh
+++ docker/php/cmd.sh
@@ -3,9 +3,9 @@
 set -e
 
 if [ "${APP_ENV}" = "production" ]; then
-    mv "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini";
+    cp -f "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini";
 else 
-    mv "$PHP_INI_DIR/php.ini-development" "$PHP_INI_DIR/php.ini";
+    cp -f  "$PHP_INI_DIR/php.ini-development" "$PHP_INI_DIR/php.ini";
 fi
 
 docker-php-entrypoint php-fpm
