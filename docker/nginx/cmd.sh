c076f272fd04d883ccd4060b0ff8a29b30e98882
Kishor Rajbanshi <kishorrajbanshi00@gmail.com>
2025-06-26 18:36:56 +0545

    WIP

--- docker/nginx/cmd.sh
+++ docker/nginx/cmd.sh
@@ -8,7 +8,7 @@ else
     ln -sf /etc/nginx/templates/default.conf /etc/nginx/templates/default.conf.template
 fi
 
-if [ "${APP_ENV}" = "local" ] && command -v nginx-debug >/dev/null; then
+if [ "${APP_DEBUG}" = "true" ] && command -v nginx-debug >/dev/null; then
     /docker-entrypoint.sh nginx-debug -g 'daemon off;'
 else
     /docker-entrypoint.sh nginx -g 'daemon off;'
