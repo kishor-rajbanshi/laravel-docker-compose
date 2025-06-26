9f06dec51388a6b5c63715e89df38ee7b16fb89e
Kishor Rajbanshi <kishorrajbanshi00@gmail.com>
2025-06-26 18:20:41 +0545

    WIP

--- docker/nginx/cmd.sh
+++ docker/nginx/cmd.sh
@@ -3,9 +3,9 @@
 set -e
 
 if [ -f /var/www/html/nginx.conf ]; then
-    ln -s /var/www/html/nginx.conf /etc/nginx/templates/default.conf.template
+    ln -sf /var/www/html/nginx.conf /etc/nginx/templates/default.conf.template
 else
-    cp -f /etc/nginx/templates/default.conf /etc/nginx/templates/default.conf.template
+    ln -sf /etc/nginx/templates/default.conf /etc/nginx/templates/default.conf.template
 fi
 
 if [ "${APP_ENV}" = "local" ] && command -v nginx-debug >/dev/null; then
