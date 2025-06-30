70611eae967b253793879ed82887c86b76715ba0
Kishor Rajbanshi <kishorrajbanshi00@gmail.com>
2025-06-30 22:00:57 +0545

    WIP

--- docker/node/healthcheck.sh
+++ docker/node/healthcheck.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ "$APP_ENV" = "local" ]; then
-  wget -q --spider http://localhost:5173 && exit 0 || exit 1
+    wget -q --spider http://localhost:5173 && exit 0 || exit 1
 else
-  [ "$IS_READY" = "true" ] && exit 0 || exit 1
+    [ "$IS_READY" = "true" ] && exit 0 || exit 1
 fi
