4ddd3e59156af757378e45ef5258a7041cbc05d5
Kishor Rajbanshi <kishorrajbanshi00@gmail.com>
2025-08-10 15:16:19 +0545

    Refractor Node service

--- docker/node/update-vite-config.sh
+++ docker/node/update-vite-config.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-CONFIG_FILE="/var/www/html/vite.config.js"
+conf_file="/var/www/html/vite.config.js"
 
-if grep -q "server:" "$CONFIG_FILE"; then
-    if grep -q "host:" "$CONFIG_FILE"; then
-        sed -i "s/host: '[^']*'/host: '0.0.0.0'/" "$CONFIG_FILE"
+if grep -q "server:" "$conf_file"; then
+    if grep -q "host:" "$conf_file"; then
+        sed -i "s/host: '[^']*'/host: '0.0.0.0'/" "$conf_file"
     else
-        sed -i '/server:/a\        host: '\''0.0.0.0'\'',' "$CONFIG_FILE"
+        sed -i '/server:/a\        host: '\''0.0.0.0'\'',' "$conf_file"
     fi
-    if grep -q "hmr: { host:" "$CONFIG_FILE"; then
-        sed -i "s/hmr: { host: '[^']*' }/hmr: { host: 'localhost' }/" "$CONFIG_FILE"
+    if grep -q "hmr: { host:" "$conf_file"; then
+        sed -i "s/hmr: { host: '[^']*' }/hmr: { host: 'localhost' }/" "$conf_file"
     else
-        sed -i '/server:/a\        hmr: { host: '\''localhost'\'' },' "$CONFIG_FILE"
+        sed -i '/server:/a\        hmr: { host: '\''localhost'\'' },' "$conf_file"
     fi
 else
-    sed -i '/export default defineConfig({/a\    server: {\n        host: '\''0.0.0.0'\'',\n        hmr: { host: '\''localhost'\'' }\n    },' "$CONFIG_FILE"
+    sed -i '/export default defineConfig({/a\    server: {\n        host: '\''0.0.0.0'\'',\n        hmr: { host: '\''localhost'\'' }\n    },' "$conf_file"
 fi
