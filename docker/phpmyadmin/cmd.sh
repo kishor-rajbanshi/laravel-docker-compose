5304b5e0ce58c90a6c78ecde6600a9b5e20b8862
Kishor Rajbanshi <kishorrajbanshi00@gmail.com>
2025-08-10 14:37:42 +0545

    Refractor Phpmyadmin service

--- docker/phpmyadmin/cmd.sh
+++ docker/phpmyadmin/cmd.sh
@@ -2,13 +2,19 @@
 
 set -e
 
+cmd_log() {
+    if [ -z "${PHPMYADMIN_CMD_QUIET_LOGS:-}" ]; then
+        echo "$@"
+    fi
+}
+
 if [ "$PHPMYADMIN_ENABLED" != "true" ] || { [ "$DB_CONNECTION" != "mysql" ] && [ "$DB_CONNECTION" != "mariadb" ]; }; then
     while true; do
         status="$(curl -s --unix-socket /var/run/docker.sock http://localhost/containers/${COMPOSE_PROJECT_NAME}-nginx/json | jq -r '.State.Status' 2>/dev/null)"
 
         [ "$status" = "running" ] && break
 
-        echo "Waiting for ${COMPOSE_PROJECT_NAME}-nginx to start"
+        cmd_log "$0: info: Waiting for ${COMPOSE_PROJECT_NAME}-nginx to start"
         
         sleep 1
     done
@@ -17,6 +23,8 @@ if [ "$PHPMYADMIN_ENABLED" != "true" ] || { [ "$DB_CONNECTION" != "mysql" ] && [
 fi
 
 if [ -f /var/www/app/config.inc.php ]; then
+    cmd_log "$0: info: Using config.inc.php from /var/www/app/."
+    
     mkdir -p /etc/phpmyadmin/conf.d
 
     ln -sf /var/www/app/config.inc.php /etc/phpmyadmin/conf.d/
