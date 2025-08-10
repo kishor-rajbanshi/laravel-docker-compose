3df44b9ebb99eb3f8ed64d14e55db694b4a1a711
Kishor Rajbanshi <kishorrajbanshi00@gmail.com>
2025-08-10 14:59:04 +0545

    Refractor Cron service

--- docker/cron/cmd.sh
+++ docker/cron/cmd.sh
@@ -2,17 +2,22 @@
 
 set -e
 
+cmd_log() {
+    if [ -z "${CRON_CMD_QUIET_LOGS:-}" ]; then
+        echo "$@"
+    fi
+}
+
 if [ "$CRON_ENABLED" != "true" ]; then
     curl -s -X DELETE --unix-socket /var/run/docker.sock "http://localhost/containers/$(hostname)?force=true"
 fi
 
 if [ -f /var/www/html/crontab ]; then
+    cmd_log "$0: info: Using crontab from $(pwd)."
     ln -sf /var/www/html/crontab /etc/crontabs/www-data
 else
+    cmd_log "$0: info: Using default crontab."
     ln -sf /etc/crontabs/crontab /etc/crontabs/www-data
 fi
 
-[ ! -p /tmp/cron-logger.pipe ] && mkfifo /tmp/cron-logger.pipe
-tee /var/log/cron.log < /tmp/cron-logger.pipe &
-
-exec crond -f -d 8 -l 8 -L /tmp/cron-logger.pipe
+exec crond -f -d 8 -l 8
