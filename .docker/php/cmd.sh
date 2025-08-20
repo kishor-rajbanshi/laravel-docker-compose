ae3fa3b109dcc390faf6807b683502f9a0eb9c2f
Kishor Rajbanshi <kishorrajbanshi00@gmail.com>
2025-08-20 12:27:55 +0545

    Refractor Php service

--- .docker/php/cmd.sh
+++ .docker/php/cmd.sh
@@ -9,15 +9,15 @@ cmd_log() {
 }
 
 if [ "${APP_ENV}" = "production" ]; then
-    cmd_log "$0: info: Using production PHP configuration."
+    cmd_log "$0: info: Using production PHP configuration"
     ln -sf "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
 else
-    cmd_log "$0: info: Using development PHP configuration."
+    cmd_log "$0: info: Using development PHP configuration"
     ln -sf "$PHP_INI_DIR/php.ini-development" "$PHP_INI_DIR/php.ini"
 fi
 
 if [ -f /var/www/html/php.ini ]; then
-    cmd_log "$0: info: Using php.ini from $(pwd)."
+    cmd_log "$0: info: Using php.ini from $(pwd)"
     ln -sf /var/www/html/php.ini "$PHP_INI_DIR/conf.d/php.ini"
 fi
 
