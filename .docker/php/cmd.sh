d81cea723d972d75dc21e0b83fff9a29187f0b38
Kishor Rajbanshi <kishorrajbanshi00@gmail.com>
2025-08-26 14:52:46 +0545

    Refractor Php service

--- .docker/php/cmd.sh
+++ .docker/php/cmd.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+php_user_ini="/var/www/html/php.ini"
+
 cmd_log() {
     if [ -z "${PHP_CMD_QUIET_LOGS:-}" ]; then
         echo "$@"
@@ -16,9 +18,9 @@ else
     ln -sf "$PHP_INI_DIR/php.ini-development" "$PHP_INI_DIR/php.ini"
 fi
 
-if [ -f /var/www/html/php.ini ]; then
-    cmd_log "$0: info: Using php.ini from $(pwd)"
-    ln -sf /var/www/html/php.ini "$PHP_INI_DIR/conf.d/php.ini"
+if [ -f "$php_user_ini" ]; then
+    cmd_log "$0: info: Using $php_user_ini"
+    ln -sf "$php_user_ini" "$PHP_INI_DIR/conf.d/php.ini"
 fi
 
 docker-php-entrypoint php-fpm
